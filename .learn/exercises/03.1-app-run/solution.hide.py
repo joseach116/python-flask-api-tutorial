4c4ab60519ca36700d0952870516bebca17ff314
joseach116 <joseluisamadochacon@gmail.com>
2025-01-28 15:36:04 +0000

    First Commit

--- .learn/exercises/03.1-app-run/solution.hide.py
+++ .learn/exercises/03.1-app-run/solution.hide.py
@@ -4,3 +4,7 @@ app = Flask(__name__)
 # These two lines should always be at the end of your app.py file
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3245, debug=True)
+
+@app.route('https://opulent-bassoon-r4vqjg7xvw4cxw9-3245.app.github.dev/', methods=['GET'])
+def hello_world():
+    return 'Hello World!'
